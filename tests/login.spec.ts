8f51fa90d7eb9035e82275e4b9656313a8f5fd26
steve.harnadi <steve.harnadi@gmail.com>
2026-05-30 12:35:59 +0700

    Refactor Login.Spec to become scalable

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,11 +1,14 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
+  // Preconditions
   await page.goto('https://www.emra.chat/login');
-  await page.getByRole('textbox', { name: 'Email' }).click();
+  
+  // Actions
   await page.getByRole('textbox', { name: 'Email' }).fill('steve.harnadi@gmail.com');
-  await page.getByRole('textbox', { name: 'Password' }).click();
   await page.getByRole('textbox', { name: 'Password' }).fill('Welcome@12345');
   await page.getByRole('button', { name: 'Sign In' }).click();
+  
+  // Assertions
   await expect(page.getByRole('heading', { name: 'Emra', exact: true })).toBeVisible();
 });
