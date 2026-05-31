456c50995f92d3c6b36b4b751a367e32135c2428
BintangS <23522016@std.stei.itb.ac.id>
2026-05-31 14:31:00 +0700

    Add Negative Test Case Scenario for Login, reduce number of browsers into default 3

--- tests/login.spec.ts
+++ tests/login.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('Scenario Login - Test Case 01 - Successfull Login @P0 @SmokeTest', async ({ page }) => {
   // Preconditions
   await page.goto('https://www.emra.chat/login');
   
@@ -11,4 +11,17 @@ test('test', async ({ page }) => {
   
   // Assertions
   await expect(page.getByRole('heading', { name: 'Emra', exact: true })).toBeVisible();
+});
+
+test('Scenario Login - Test Case 02 - Invalid Credentials Failed Login @P0 @SmokeTest', async ({ page }) => {
+  // Preconditions
+  await page.goto('https://www.emra.chat/login');
+  
+  // Actions
+  await page.getByRole('textbox', { name: 'Email' }).fill('wrongemail@gmail.com');
+  await page.getByRole('textbox', { name: 'Password' }).fill('abcdefg@123445');
+  await page.getByRole('button', { name: 'Sign In' }).click();
+
+  // Assertions
+  await expect(page.getByText('Invalid credentials')).toBeVisible();
 });
