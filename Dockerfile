64c61228881726a87dd2aebe637656d5cb0f4013
Steve Immanuel Harnadi <steve.harnadi@gmail.com>
2026-06-20 15:48:52 +0700

    Fix playwright.yml and DockerFile

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,10 @@ WORKDIR /app
 
 ENV PATH /app/node_modules/.bin:$PATH
 ENV PLAYWRIGHT_BROWSERS_PATH=/ms-playwright
+ENV BASE_URL=https://www.emra.chat
+ENV WORKERS=6
+ENV RETRY=2
+ENV CI=true
 
 COPY package*.json ./
 RUN npm ci
--- playwright.config.ts
+++ playwright.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.RETRY ? parseInt(process.env.RETRY) : 0,
+  retries: process.env.RETRY ? parseInt(process.env.RETRY) : 2,
   /* Opt out of parallel tests on CI. */
   workers: process.env.WORKERS ? parseInt(process.env.WORKERS) : 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
